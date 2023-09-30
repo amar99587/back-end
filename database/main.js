ec670c0f7a0aea00c660b6897ae4302ac7c169cc
amar99587 <trissiti19@gmail.com>
2023-09-30 23:27:58 +0100

    edit connection db problem

--- database/main.js
+++ database/main.js
@@ -1,11 +1,21 @@
-const { Client } = require("pg");
+const { Pool } = require("pg");
 
-const client = new Client({
-    port: process.env.db_port,
-    host: process.env.db_host,
-    user: process.env.db_user,
-    database: process.env.db_name,
-    password: process.env.db_password,
-});
+const connectionString = process.env.connectionString;
+
+const client = new
+    // Client({
+    //     port: process.env.db_port,
+    //     host: process.env.db_host,
+    //     user: process.env.db_user,
+    //     database: process.env.db_name,
+    //     password: process.env.db_password,
+    // });
+
+    Pool({
+        connectionString,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
 
 module.exports = client;
